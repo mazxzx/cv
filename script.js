b5bc0baac309a93e43149b0b537c03e8fec39a71
yo <you@example.com>
2023-02-20 23:20:53 -0300

    educacion + inicio footer

--- script.js
+++ script.js
@@ -7,10 +7,8 @@ async function load(){
 
     document.getElementById('nombre').innerHTML = names.results[0].name.first + ' ' + names.results[0].name.last;
     document.getElementById('numero').innerHTML =names.results[0].phone + '|' + names.results[0].cell;
-		
     document.getElementById('email').innerHTML = names.results[0].email;
-    //document.getElementById('user_name1').innerHTML =  '@' + names.results[0].name.first + names.results[0].name.last + get_year(names);
-    //document.getElementById('user_name2').innerHTML =  '@' + names.results[0].name.first + names.results[0].name.last + get_year(names);
+    document.getElementById('thumbnail').src =names.results[0].picture.thumbnail;
     document.getElementById('foto_perfil').src = names.results[0].picture.large;
     document.getElementById('timezone').innerHTML = 'UTC ' + names.results[0].location.timezone.offset;
     document.getElementById('calle').innerHTML = names.results[0].location.street.name;
